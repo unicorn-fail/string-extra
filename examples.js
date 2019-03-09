d92552b75ee28c5f13a8c3c7898a6a460c561119
Mark Carver <mark.carver@me.com>
2019-03-09 15:35:33 -0600

    Fix typo

--- examples.js
+++ examples.js
@@ -4,4 +4,4 @@ console.log(S('This supports %s placeholders that %s to %s.', 'multiple', 'defau
 console.log(S('Does it support numeric %d, %i, %.03f and other basic placeholders as well? %t', -42.123456789, 42.123456789, 42.123456789, true).yellow.s);
 console.log(S('Have a simple %T? It supports inline JSON %j placeholders too.', obj, obj).green.s);
 console.log(S('Strings can have a prefix or a suffix values with their own styles! %s', '(useful for logging)').blue.args.dim.prefix(S('[prefix]').bgBlack.whiteBright.bold).suffix(S('[suffix]').bgBlack.whiteBright.bold).s);
-console.log(S('Placeholders %s can have the own %s too!', 'values', 'styles').magenta.args.cyanBright.bold.underline.s);
+console.log(S('Placeholders %s can have their own %s too!', 'values', 'styles').magenta.args.cyanBright.bold.underline.s);
